860bebacb239c9a0b68e0da595fa5bad377dd01d
bhanushalimahesh3 <bhanushali.mahesh3@gmail.com>
2020-08-17 21:38:56 +0530

    comment added

--- src/Console/TransformerCommand.php
+++ src/Console/TransformerCommand.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace BhanushaliMahesh\TransformerPackage\Console;
 
 use Illuminate\Console\GeneratorCommand;
@@ -7,25 +6,29 @@ use Illuminate\Support\Facades\File;
 
 class TransformerCommand extends GeneratorCommand
 {
+
     protected $name = 'transformer-package:transformer-create';
 
     protected $description = 'Create a new transformer class';
 
     protected $type = 'Transformer';
 
-    const  DEFAULT_FOLDER= 'Transformer';
+    const  DEFAULT_FOLDER = 'Transformer';
     const  USE_CLASS_PLACEHOLDER = 'DummyExtend';
     const  STUB_FOLDER = '/stubs/transformers/';
+    
 
     protected function getStub()
     {
-        return __DIR__ . self::STUB_FOLDER . 'transformer.php.stub';
+        return __DIR__.self::STUB_FOLDER.'transformer.php.stub';
     }
+
 
     protected function getDefaultNamespace($rootNamespace)
     {
-        return $rootNamespace . "\\" . config('transformer_package.transformer.path') . '\\' . self::DEFAULT_FOLDER;
+        return $rootNamespace."\\".config('transformer_package.transformer.path').'\\'.self::DEFAULT_FOLDER;
     }
+
 
     public function handle()
     {
@@ -34,27 +37,29 @@ class TransformerCommand extends GeneratorCommand
         $this->replaceExtendPlaceholder();
     }
 
+
     protected function replaceExtendPlaceholder()
     {
-        // replace use class
+        // replace use class.
         $usePlaceholder = $this->qualifyClass(self::DEFAULT_FOLDER);
         
-        // Get the fully qualified class name (FQN)
+        // Get the fully qualified class name (FQN).
         $class = $this->qualifyClass($this->getNameInput());
        
-        // get the destination path, based on the default namespace
+        // get the destination path, based on the default namespace.
         $path = $this->getPath($class);
         
         $content = file_get_contents($path);
 
         $content = str_replace('DummyExtend', $usePlaceholder, $content);
        
-        // Update the file content with additional data (regular expressions)
+        // Update the file content with additional data (regular expressions).
 
         file_put_contents($path, $content);
 
         $this->createTransformBase();
     }
+
 
     private function createTransformBase()
     {
@@ -62,12 +67,14 @@ class TransformerCommand extends GeneratorCommand
         $transformerBaseClassPath = $this->getPath($transformerBaseClass);        
         $transformerBaseClassNamespace = $this->getNamespace($transformerBaseClass);
     
-        // make sure we're starting from a clean state
-        if (!File::exists($transformerBaseClassPath)) {
-            $content = file_get_contents(__DIR__ . self::STUB_FOLDER . 'base.php.stub');
+        // make sure we're starting from a clean state.
+        if (File::exists($transformerBaseClassPath) === false) {
+            $content = file_get_contents(__DIR__.self::STUB_FOLDER.'base.php.stub');
             $content = str_replace('DummyNamespace', $transformerBaseClassNamespace, $content);
             file_put_contents($transformerBaseClassPath, $content);        
             
         }
-    }
+    } //end
+
+
 }
